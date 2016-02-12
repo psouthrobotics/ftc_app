103a4f7edd767b36a30625fa3e08746f6bb8dbb8
Drew Zoghby <dzoghby4@gmail.com>
2016-02-11 20:26:06 -0600

    Changed to OOP based library and orginized code

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DriveWithoutEncoders.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DriveWithoutEncoders.java
@@ -2,107 +2,76 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 
 
-public abstract class DriveWithoutEncoders extends LinearOpMode {
+public class DriveWithoutEncoders extends LinearOpMode {
     DcMotor leftMotor;
     DcMotor rightMotor;
     DcMotor leftTank;
     DcMotor rightTank;
-    Servo servo;
 
     GyroSensor gyro;
+
+    public DriveWithoutEncoders() {
+
+    }
+    //To satisfy interface
+    public void runOpMode(){}
+
     //t is time x is direction
-    public void go(double t, double x) throws InterruptedException{
-        //assign hardware to objects
-        leftMotor = hardwareMap.dcMotor.get("left_Motor");
-        rightMotor = hardwareMap.dcMotor.get("right_Motor");
-        leftTank = hardwareMap.dcMotor.get("left_tank");
-        rightTank = hardwareMap.dcMotor.get("right_tank");
-        gyro = hardwareMap.gyroSensor.get("gy");
-        servo = hardwareMap.servo.get("servo");
+    public void goT(double time, double turningRate) throws InterruptedException{
+
+        mapHardware();
         //reversing motors as needed
-        rightMotor.setDirection(DcMotor.Direction.REVERSE);
-        rightTank.setDirection(DcMotor.Direction.FORWARD);
-        leftTank.setDirection(DcMotor.Direction.FORWARD);
-        leftMotor.setDirection(DcMotor.Direction.FORWARD);
-
-
-        //defining variables
-        double kp;
-        double ki;
-        double kd;
-        double error;
-        double previous_error;
-        double proportional;
-        double intergral;
-        double derivitive;
-        double start_time;
-        double ld_speed;
-        double rd_speed;
-        double straight;
-        long dt;
-        double PID;
-        double angle;
+        setDirection();
         //setting straight value
         //below is left above is right
-        straight = 575 + x;
+        double straight = 575 + turningRate;
         //setting drive speed
-        ld_speed = 0.6;
-        rd_speed = 0.6;
-        //how often to run the loop
-        dt = 20;
+        double lSpeed = 0.6;
+        double rSpeed = lSpeed;
+        //How long to sleep between loop  cycles to help with math
+        long dt = 20;
         //coefficients for PID loop
-        kp = 0.005;
-        ki = 0.0025;
-        kd = 6;
-        //start time to compare against
-        start_time = System.currentTimeMillis();
-        //setting variables to zero to use in first loop round
-        intergral = 0;
-        previous_error = 0;
-        angle = 0;
-
-        leftMotor.setPower(ld_speed);
-        rightMotor.setPower(rd_speed);
-        leftTank.setPower(ld_speed);
-        rightTank.setPower(rd_speed);
-        telemetry.addData("Begining ", "Loop");
-
-        sleep(500);
-        while (System.currentTimeMillis() - start_time < t) {
-            angle = angle + gyro.getRotation()/dt;
-            error = straight - gyro.getRotation();
-            //dividing errror because motor speed is in percentage
+        //                      P        I    D
+        double[] pidValues = {0.0025, 0.0017, 3};
+        //start time to compare against so loop only runs for so long
+        double start_time = System.currentTimeMillis();
+        //setting variables to zero to use in first loop round to avoid NULL errors
+        double integral = 0;
+        double previous_error = 0;
+        //timer to run loop for given amount of time
+        while (System.currentTimeMillis() - start_time < time) {
+            //error is how far off a straight value is to use for calculating corrections
+            double error = straight - gyro.getRotation();
+            //dividing error because motor speed is in percentage
             error = error / 1000;
-            //proportional which is just error
-            proportional = error;
-            //intergral art of loop- error over time
-            intergral = intergral + error * dt;
-            //derivitive which uses slope to correct future error
-            derivitive = (error - previous_error) / dt;
-            //suming together to create complete correction value
-            PID = kp * (proportional + ki * intergral + kd * derivitive);
-            //applying corrections to driving
-            ld_speed = ld_speed + PID;
-            rd_speed = rd_speed - PID;
-            ld_speed = Range.clip(ld_speed, 0, 1);
-            rd_speed = Range.clip(rd_speed, 0, 1);
-            leftMotor.setPower(ld_speed);
-            rightMotor.setPower(rd_speed);
-            leftTank.setPower(ld_speed);
-            rightTank.setPower(rd_speed);
+            //proportional factor so correction is relative size of error
+            double proportional = error;
+            //integral helps deal with drift by calculating error over time and builds as the loop goes to deal with uncorrected error
+            integral = integral + error * dt;
+            //derivative which uses slope of the error to correct future error and to prevent overshooting
+            double derivative = (error - previous_error) / dt;
+            //summing together to create complete correction value and multiplying by coefficients
+            double PID = pidValues[0] * proportional + pidValues[1] * integral + pidValues[2] * derivative;
+            //applying corrections to driving so we go straight
+            lSpeed = lSpeed + PID;
+            rSpeed = rSpeed - PID;
+            lSpeed = Range.clip(lSpeed, 0, 1);
+            rSpeed = Range.clip(rSpeed, 0, 1);
+            leftMotor.setPower(lSpeed);
+            rightMotor.setPower(rSpeed);
+            leftTank.setPower(lSpeed);
+            rightTank.setPower(rSpeed);
 
-            //setting vlues for next loop
+            //setting values for next loop so integral roles over
             previous_error = error;
             //telemetry stuff
-            telemetry.addData("Left Drive", ld_speed);
-            telemetry.addData("Right Drive", rd_speed);
+            telemetry.addData("Left Drive", lSpeed);
+            telemetry.addData("Right Drive", rSpeed);
             telemetry.addData("Rotation", gyro.getRotation());
-            telemetry.addData("Angle", angle);
 
             sleep(dt);
         }
@@ -113,6 +82,19 @@ public abstract class DriveWithoutEncoders extends LinearOpMode {
         rightMotor.setPower(0);
         leftTank.setPower(0);
         rightTank.setPower(0);
+    }
+    public void mapHardware(){
+        leftMotor = hardwareMap.dcMotor.get("left_Motor");
+        rightMotor = hardwareMap.dcMotor.get("right_Motor");
+        leftTank = hardwareMap.dcMotor.get("left_tank");
+        rightTank = hardwareMap.dcMotor.get("right_tank");
+        gyro = hardwareMap.gyroSensor.get("gy");
+    }
+    public void setDirection(){
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightTank.setDirection(DcMotor.Direction.FORWARD);
+        leftTank.setDirection(DcMotor.Direction.FORWARD);
+        leftMotor.setDirection(DcMotor.Direction.FORWARD);
     }
 
 }
